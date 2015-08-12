f9e3b0ce97de16dd44645deb92a69e7d798688e9
Alex Costa & Filipe Silvestre <student@epicodus.com>
2015-08-12 12:18:50 -0700

    Add Object functionality

--- src/RPS.php
+++ src/RPS.php
@@ -1,14 +1,26 @@
 <?php
 class RPS
 {
+    private $player_weapon;
+    private $weapons = array(
+        array(1,2,"rock"),
+        array(2,3,"paper"),
+        array(3,1,"scissors")
+        );
+
+    function __construct($weapon_choice) {
+        $this->player_weapon = $weapons[$weapon_choice];
+    }
+
+    function getPlayerWeapon() {
+        return $this->player_weapon;
+    }
+
+
     function RPS_game($player1_input, $player2_input)
     {
         // Values for the weapons
-        $weapons = array(
-            array(1,2,"rock"),
-            array(2,3,"paper"),
-            array(3,1,"scissors")
-            );
+
 
         // Player weapons
         $player1_weapon = $weapons[$player1_input];
